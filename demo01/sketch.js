86710abe89fd1faae998411dfa5c4426dc08d7c2
melodyloveless <5409179+melodyloveless@users.noreply.github.com>
2021-09-15 15:11:05 -0400

    Add files via upload

---
+++ demo01/sketch.js
@@ -0,0 +1,47 @@
+
+
+
+function setup() {
+  // this is a comment
+  /// createCanvas(width, height);
+  createCanvas(560, 400);
+}
+
+function draw() {
+  // //// R G B
+  background(5, 55, 40);
+  /// width = system variable -> current width of the canvas
+  ellipse(mouseX, mouseY, 50, 50);
+
+  arc(50, 50, 80, 20, 0, PI + QUARTER_PI);
+
+  // // ellipse = circle
+  // // x position, y position, width, height
+  //
+  // fill(0, 0, 255);
+  // strokeWeight(3);
+  // stroke(255, 0, 0); // stroke - color of outline
+  // rect(100, 100, 250, 80);
+  //
+  // noFill();
+  // ellipse(250, 200, 100, 100);
+  //
+  // // first point x, first point y, second point x, second point y, third point x, third point y
+  // triangle(250, 10, 100, 250, 300, 250);
+  //
+  // strokeWeight(10);
+  // stroke(255, 255, 0);
+  // line(0, 200, 300, 320);
+  //
+  // // 'string [text you want to display]', x, y
+  // strokeWeight(5);
+  // stroke(0, 0, 255);
+  // textSize(120);
+  // fill(0, 255, 0);
+  // text("hello", 250, 200);
+  // textAlign("center");
+
+
+
+
+}
